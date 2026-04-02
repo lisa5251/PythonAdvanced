b53bdcef42dd2af864ca4f7ce3377af741b3709d
lisa5251 <suliqlisa@gmail.com>
2026-04-02 20:46:51 +0200

    lesson 1

---
+++ lesson 1/main.py
@@ -0,0 +1,24 @@
+name = "lisa"
+surname=(" suliqi")
+age = 15
+temeprature = 36
+
+full_name= name+surname
+users=["LISA","Andi","Edeni","Olti","LISA"]
+
+users.insert(2,"lisa")
+
+# print(users)
+# users.reverse()
+# print(users)
+# users.append("Roberti")
+# print(users.count("LISA"))
+# users.clear()
+#print(name,age,temeprature)
+#print(full_name)
+#print(type(name))
+# print(users[0])
+# print(users[1])
+# print(users[2])
+# print(users[3])
+# print(users)
