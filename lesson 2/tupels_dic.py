2ad59e6719758dde73903e5836dd8b66bb7c2957
lisa5251 <suliqlisa@gmail.com>
2026-04-14 20:47:51 +0200

    hshsh

---
+++ lesson 2/tupels_dic.py
@@ -0,0 +1,12 @@
+grades ={
+    ("Jon","Math"):4,
+    ("Robert", "Haxhiu"): 5,
+}
+
+print(grades[("Robert","Math")])
+
+grades[("Jon","Math")]=2
+print(grades[("Jon","Math")])
+
+
+
---
+++ lesson 2/tuples.py
@@ -0,0 +1,36 @@
+# print("Hello")
+
+# names = ("spam", "hello","nili")
+#
+# print(names[2])
+#
+# empty_tp=()
+#
+# print(empty_tp)
+
+person = ("nili", 15, "pilot")
+
+name,age,profesioni=person
+
+print(name)
+print(age)
+print(profesioni)
+
+numrat = {
+"KS":"+383",
+"IT":"+39",
+"AL":"+355"}
+
+kosova = numrat["KS"]
+print(kosova)
+
+print(numrat["AL"])
+
+numrat["KS"]="+299"
+
+print(numrat)
+
+
+
+
+
