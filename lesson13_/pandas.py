72ac5c77e24316e37ac162c7258a6a1f4fbc9f6e
lisa5251 <suliqlisa@gmail.com>
2026-05-21 20:46:05 +0200

    lesson13

---
+++ lesson13_/pandas.py
@@ -0,0 +1,16 @@
+from itertools import product
+
+import pandas as pd
+from array_manipulator import total_sum
+
+product = ["Apples","Bannanas","Oranges","Grapes","Pineapples"]
+
+sales = [150,200,180,90,60]
+
+sales_series = pd.Series(sales, index=product)
+
+print(sales_series)
+
+print(sales_series['Grapes'])
+
+total_sum
